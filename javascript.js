37403fdc7b038fe5c66a7290961cd951e1d8fa89
Sebastian Miranda <juansebastianmirandac@gmail.com>
2025-12-18 22:58:52 -0500

    Updated to function as a full game of 5 rounds

--- javascript.js
+++ javascript.js
@@ -1,3 +1,9 @@
+alert("Hi babe you're about to play Rock, papper, scissors! It will be 5 rounds! GO!!");
+let humanScore = 0;
+let computerScore = 0;
+
+function playGame(playRound) {
+
 function getHumanChoice() {
     let choice = prompt("rock, paper , scissors!");
     return choice.trim().toLowerCase();
@@ -17,7 +23,15 @@ function getComputerChoice(){
 }
 
 
-function playRound(getHumanChoice, getComputerChoice) {
+const humanSelection = getHumanChoice();
+console.log("You chose:", humanSelection);
+const computerSelection = getComputerChoice();
+console.log("The computer chose:", computerSelection); 
+
+
+playRound(humanSelection, computerSelection);
+
+  function playRound(getHumanChoice, getComputerChoice) {
   if ( getHumanChoice === "rock" &&  getComputerChoice === "paper") {
     console.log( "You lose! paper beats rock!");
     computerScore++;
@@ -45,19 +59,24 @@ function playRound(getHumanChoice, getComputerChoice) {
   } else if (getHumanChoice === getComputerChoice) {
    console.log("It's a tie!")
   }
-
+}
 }
 
 
-let humanScore = 0;
-let computerScore = 0;
-const humanSelection = getHumanChoice();
-console.log("You chose:", humanSelection);
-const computerSelection = getComputerChoice();
-console.log("The computer chose:", computerSelection);
+for (let i = 0; i < 5; i++) {
+  playGame();
+  console.log(`Your score:`, humanScore);
+  console.log(`Computer score:`, computerScore);
+}
 
+winner();
 
-playRound(humanSelection, computerSelection);
-
-console.log("The computer score is:", computerScore);
-console.log("Your score is:", humanScore);
+function winner() {
+    if (humanScore > computerScore) {
+        console.log("YASS YOU ARE THE WINNER, MOTHERRRR");
+    } else if (humanScore < computerScore) {
+        console.log("YOU LOSE! Girl , the computer ate you!!");
+    } else if (humanScore === computerScore) {
+        console.log("WTF it's a tie!!");
+    }
+}
